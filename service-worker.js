b7b2678d5e52537451eb93a04ddb67801c67087c
1carog <icarob.dossantos@outlook.com>
2025-10-06 15:34:08 -0300

    sexto commit

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open("app-cache").then((cache) => {
-      return cache.addAll(["/", "/index.html", "/css/style.css", "/js/script.js"]);
+      return cache.addAll(["/index.html", "/css/style.css", "/js/script.js"]);
     })
   );
 });
